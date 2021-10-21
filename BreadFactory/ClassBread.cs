ef08ec3309378fe6d33832984a1612a0fa8e4aa4
Jenuskii <jenuskii317@gmail.com>
2021-10-21 18:34:50 +0900

    Initial commit

---
+++ BreadFactory/ClassBread.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace BreadFactory
+{
+    public abstract class BaseBread
+    {
+        public abstract void Show();
+        
+        public abstract JObject GetData();
+          
+    }
+
+    public class Bread : BaseBread
+    {
+        public JObject Data = new JObject();
+        public string BreadType;
+        
+        public Bread(Recipe BreadRecipe)
+        {
+            BreadType = BreadRecipe.Name;
+            Data.Add("breadType", BreadType);
+            Data.Add("recipe", BreadRecipe.GetRecipe());
+
+        }
+
+        
+        public override void Show()
+        {
+            string Original = Data.ToString();
+            string[] Target = {": {", "{", "  ", "\"", ",", "}"};
+            foreach (string i in Target)
+            {
+                Original = Original.Replace(i, "");
+            }
+            Console.WriteLine(Original);
+
+        }
+
+        public override JObject GetData()
+        {
+            return Data;
+        }
+
+    }
+
+
+}
---
+++ BreadFactory/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BreadFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recipe cream = new Recipe("cream", 100, 100, 200);
+            Recipe sugar = new Recipe("sugar", 100, 50, 200);
+            Recipe butter = new Recipe("butter", 100, 100, 200);
+
+            List<Bread> BreadData = new List<Bread>();
+            //Bread Cream = new Bread(cream);
+            //Bread Sugar = new Bread(sugar);
+            //Bread Butter = new Bread(butter);
+
+            BreadData.Add(new Bread(cream));
+            BreadData.Add(new Bread(sugar));
+            BreadData.Add(new Bread(butter));
+
+
+            foreach (Bread EachBread in BreadData)
+            {
+                EachBread.Show();
+            }
+
+        }
+    }
+}
